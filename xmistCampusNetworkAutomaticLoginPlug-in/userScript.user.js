109632ccad0c1a8fb9ee35ac21904649a19a17c1
Aigio1064 <97342833+Aigio1064@users.noreply.github.com>
2022-06-24 04:28:33 +0800

    Add files via upload

--- xmistCampusNetworkAutomaticLoginPlug-in/userScript.user.js
+++ xmistCampusNetworkAutomaticLoginPlug-in/userScript.user.js
@@ -1,9 +1,11 @@
 // ==UserScript==
 // @name         厦门软件职业技术学院校园网自动登录插件
 // @namespace    https://tampermonkey.ultiaigio.top/xmistCampusNetworkAutomaticLoginPlug-in/
-// @version      1.1
+// @version      1.2
 // @description  只需要完成一次登录操作，在你下次打开登录校园网页面时，将会自动为你登录，并且在自动完成登录操作后尝试关闭页面。如果想更换校园网账户，那么请先把此脚本禁用。
 // @author       Aigio1064
+// @license      GNU General Public License v3.0
+// @updateURL    https://tampermonkey.ultiaigio.top/xmistCampusNetworkAutomaticLoginPlug-in/userScript.user.js
 // @match        http://172.172.255.20/eportal/index.jsp*
 // @match        http://172.172.255.20/eportal/success.jsp*
 // ==/UserScript==
